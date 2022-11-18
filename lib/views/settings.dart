88bb124c74011de89f8b9ae318076c8c244f7748
3nt3 <gott@3nt3.de>
2022-11-18 21:07:26 +0100

    settings page layout begin
    Signed-off-by: 3nt3 <gott@3nt3.de>
    Co-authored-by: Sanberk <sanberk@sanberk.xyz>

--- lib/views/settings.dart
+++ lib/views/settings.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:tutoring_mobile/components/bottom_nav_bar.dart';
 
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 class MySettingsPage extends StatelessWidget {
   const MySettingsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("settings"),
+    return SafeArea(
+      child: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(AppLocalizations.of(context)!.settingsTitle,
+                style: Theme.of(context).textTheme.headline4),
+          ),
+          ListTile(
+            leading: const Icon(Icons.person),
+            title: Text(AppLocalizations.of(context)!.profile),
+            onTap: () {
+              print("profile bearbeiten");
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.school),
+            title: Text(AppLocalizations.of(context)!.listings),
+            onTap: () {
+              print("cool");
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.lock_outlined),
+            title: Text(AppLocalizations.of(context)!.account),
+            onTap: () {
+              print("wuff wuff");
+            },
+          )
+        ],
+      ),
     );
   }
 }
